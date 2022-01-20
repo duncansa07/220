ca2f082ccebaae44d43f9ce4313bdc3b82b0201c
duncansa07 <duncansa07@gmail.com>
2022-01-19 21:04:37 -0500

    Completed HW 1

--- assignments/hw1/hw1.py
+++ assignments/hw1/hw1.py
@@ -1,34 +1,47 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Scarlett Duncan
+hw1.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: We are using this code to practice using arithmatic and inputs to create outputs.
+It is used as an overview of using pycharm and pushing to gitHub.
 
 Certification of Authenticity:
-<include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
 
 
 def calc_rec_area():
-    pass
+    length = eval(input("Enter the length: "))
+    width = eval(input("Enter the width: "))
+    area = length * width
+    print("Area =", area)
 
 
 def calc_volume():
-    pass
+    length = eval(input("Enter the length: "))
+    width = eval(input("Enter the width: "))
+    height = eval(input("Enter the height: "))
+    volume = length * width * height
+    print("Volume =", volume)
 
 
 def shooting_percentage():
-    pass
+    total_shots = eval(input("Enter the player's total shots: "))
+    made_shots = eval(input("Enter how many shots the player made: "))
+    shot_percentage = (made_shots/total_shots) * 100
+    print("Shooting Percentage: ", shot_percentage, "%")
 
 
 def coffee():
-    pass
+    amt_coffee = eval(input("How many pounds of coffee would you like?"))
+    shipping_total = (amt_coffee * 10.5) + (amt_coffee * .86) + 1.5
+    print("Your total is: $", shipping_total)
 
 
 def kilometers_to_miles():
-    pass
+    km_traveled = eval(input("How many kilometers did you travel?"))
+    miles_traveled = km_traveled/1.61
+    print("That's", miles_traveled, "miles!")
 
 
 if __name__ == '__main__':
