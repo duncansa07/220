2211b3adbd6a1516a2e58d4c0af23978930886e6
duncansa07 <duncansa07@gmail.com>
2022-01-27 22:35:09 -0500

    Completed hw 2

--- assignments/hw2/hw2.py
+++ assignments/hw2/hw2.py
@@ -1,35 +1,56 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Scarlett Duncan
+hw2.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: This program is designed to perform various arithmatic problems.
 
 Certification of Authenticity:
-<include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
 import math
 
 
 def sum_of_threes():
-    pass
+    upper_bound = eval(input("What is the upper bound?"))
+    acc_threes = 0
+    for i in range(0, upper_bound+1, 3):
+        acc_threes = acc_threes + i
+    print(acc_threes)
 
 
 def multiplication_table():
-    pass
+    for i in range(1, 11):
+        for j in range(1, 11):
+            multiplication = i * j
+            print(multiplication, end="\t")
+        print()
 
 
 def triangle_area():
-    pass
+    side_a = eval(input("Enter side a length: "))
+    side_b = eval(input("Enter side b length: "))
+    side_c = eval(input("Enter side c length: "))
+    sides = (side_a + side_b + side_c) / 2
+    area = math.sqrt(sides * (sides - side_a) * (sides - side_b) * (sides - side_c))
+    print("area is ", area)
 
 
 def sum_squares():
-    pass
+    lower_limit = eval(input("Enter lower range: "))
+    upper_limit = eval(input("Enter upper range: "))
+    acc_squares = 0
+    for i in range(lower_limit, upper_limit+1):
+        acc_squares = (i * i) + acc_squares
+    print(acc_squares)
 
 
 def power():
-    pass
+    base = eval(input("Enter base: "))
+    exponent = eval(input("Enter exponent: "))
+    acc_power = 1
+    for _ in range(exponent):
+        acc_power = base * acc_power
+    print(base, "^", exponent, "=", acc_power)
 
 
 if __name__ == '__main__':
