b66d61ccf83e6db6be42b8da654afbd5f0459b4a
duncansa07 <duncansa07@gmail.com>
2022-02-17 23:47:35 -0500

    Completed HW 5

--- assignments/hw5/hw5.py
+++ assignments/hw5/hw5.py
@@ -1,42 +1,91 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Scarlett Duncan
+hw5.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: Create codes to practice using strings to produce various outputs
 
 Certification of Authenticity:
-<include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
+
 """
 
 
 def name_reverse():
-    pass
+    name = input("enter a name (first last): ")
+    split = name.split()
+    print(split[-1], ",", split[0])
 
 
 def company_name():
-    pass
+    domain = input("enter a domain:" )
+    company = domain.split(".")
+    print(company[1])
 
 
 def initials():
-    pass
+    student_number = eval(input("how many students are in the class?"))
+    for i in range(student_number):
+        words = "what is the name of student " + str(i+1) + "?"
+        name = input(words)
+        name_seperate = name.split()
+        first_name = name_seperate[0]
+        last_name = name_seperate[-1]
+        initial_first = first_name[0]
+        initial_last = last_name[0]
+        print(initial_first+initial_last)
+
 
 
 def names():
-    pass
-
+    names_list = input("Enter a list of names: ")
+    names_split = names_list.split(",")
+    number = len(names_split)
+    for i in range(number):
+        name_seperate = names_split[i]
+        name_split_again = name_seperate.split()
+        first_name = name_split_again[0]
+        last_name = name_split_again[-1]
+        first_initial = first_name[0]
+        last_initial = last_name[0]
+        print(first_initial+last_initial, end=" ")
 
 def thirds():
-    pass
+    sentence_number = eval(input("enter the number of sentences:"))
+    acc = " "
+    for i in range(sentence_number):
+        words = "enter sentence " + str(i+1) +":"
+        sentence = input(words)
+        every_third = sentence[0:-1:3]
+        acc = acc + every_third + "\n"
+    print(acc)
 
 
 def word_average():
-    pass
+    sentence = input("enter a sentence: ")
+    sentence_split = sentence.split()
+    word_count = len(sentence_split)
+    acc = 0
+    for i in range(word_count):
+        word = sentence_split[i]
+        letter_count = len(word)
+        acc = acc + letter_count
+    average = acc / word_count
+    print(average)
 
 
 def pig_latin():
-    pass
+    sentence = input("enter a sentence to convert to pig latin: ")
+    sentence_split = sentence.split()
+    number_words = len(sentence_split)
+    acc = " "
+    for i in range(number_words):
+        word = sentence_split[i]
+        first_letter = word[0]
+        other_letters = word[1:]
+        ending = "ay"
+        pig_latin = other_letters + first_letter + ending
+        acc = acc + " " + pig_latin
+    print(acc)
 
 
 if __name__ == '__main__':
